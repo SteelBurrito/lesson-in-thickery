674279cffa40437d526a29f1a348871a28c09603
SteelBurrito <antouniokevin@gmail.com>
2017-12-27 10:47:00 +0700

    setting up vuejs environment

---
+++ src/components/Translate.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <h1>THICC TRANSLATOR</h1>
+        </div>
+        <div class="row justify-content-md-center">
+            <div class="form">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="textInput">Enter Text</label>
+                        <textarea v-model="textInput" class="form-control" id="textInput" rows="3"></textarea>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="textOutput">Translated Text</label>
+                        <textarea v-model="textOutput" class="form-control-plaintext" id="textOutput" rows="3" readonly></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      textInput: '',
+      textOutput: '',
+    };
+  },
+  methods: {
+    textTranslate() {},
+  },
+};
+</script>
